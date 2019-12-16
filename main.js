25ae225dd08a13da3ecf5975cf7246eb1bfded98
erickSosa <ericksosagarcias@gmail.com>
2019-12-15 20:19:00 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        test : 'Domino',
+        nombres: ['erick', 'juan', 'pedro'],
+        jugadores: [{nombre: 'Equipo 1', total: 0, res: 0},
+                {nombre: 'Equipo 2', total: 0, res: 0}],
+        string: '',
+        total: 10,
+        mensage: 'Hecho con amor por Erick'
+    },
+    methods: {
+    }
+});
